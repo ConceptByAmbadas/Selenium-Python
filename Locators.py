9e7509a5b8cb22c0a788be5b57693e665fc7eaef
AmbadasAdhav <ambadasadhav@yahoo.com>
2018-07-10 23:15:34 +0530

    new commit

---
+++ Locators.py
@@ -0,0 +1,19 @@
+
+
+class AllLocators(object):
+    title="title"
+    first_name="first_name"
+    last_name="last_name"
+    email="email"
+    phone="phone"
+    address="address"
+    city="city"
+    state="state"
+    zip="//input[@name='zip']"
+    username="//input[@name='username']"
+    password="//input[@name='password']"
+    hosting="//input[@name='hosting']"
+    comment="comment"
+    btn_submit="btn_submit"
+   
+    
